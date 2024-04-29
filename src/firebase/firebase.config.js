ab8b4a655a556d4e4d235415b8e003de8b6b5a76
rishowrav <rishowrav@gmail.com>
2024-04-29 07:53:20 +0600

    Added Environment variable to hide the firebase confg keys

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD3SzV5KP_FEf9rM9s-kKk5dGqGDieBg6s",
-  authDomain: "pottery-project-d0f5c.firebaseapp.com",
-  projectId: "pottery-project-d0f5c",
-  storageBucket: "pottery-project-d0f5c.appspot.com",
-  messagingSenderId: "688699539136",
-  appId: "1:688699539136:web:781bce4b7adcab6bfd7725",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
--- src/privateRoute/PrivateRoute.jsx
+++ src/privateRoute/PrivateRoute.jsx
@@ -2,8 +2,8 @@ import PropTypes from "prop-types";
 import { useContext } from "react";
 import { AuthContext } from "../authProvider/AuthProvider";
 import { Navigate, useLocation } from "react-router-dom";
-// import Lottie from "lottie-react";
-// import Parrot from "../../public/Parrot.json";
+import Lottie from "lottie-react";
+import Parrot from "../../public/Parrot.json";
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
@@ -12,10 +12,10 @@ const PrivateRoute = ({ children }) => {
   if (loading) {
     return (
       <div className="w-[200px] mx-auto">
-        {/* <Lottie loop={true} animationData={Parrot}></Lottie> */}
-        <h1 className="text-4xl font-bold text-center h-[400px] flex items-center justify-center">
+        <Lottie loop={true} animationData={Parrot}></Lottie>
+        {/* <h1 className="text-4xl font-bold text-center h-[400px] flex items-center justify-center">
           <span>Loading.....</span>
-        </h1>
+        </h1> */}
       </div>
     );
   }
